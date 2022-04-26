fa39576351c22d7bbdc5157c4378ccb30fa42efc
Micah Jackson <thehazelwize@gmail.com>
2022-04-25 20:40:47 -0400

    refactored for dry code

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-
+//DOM Manipulation variables
 let img = document.querySelector('img')
 let h2 = document.querySelector('h2')
 let desc = document.querySelector('p')
+
+//Variables used for switching between single phot or slideshow
 let slideshow = document.querySelector('#slideshow')
 let date = document.querySelector('#date');
+
+//Date of the displayed photo
 let currentDate;
 
 document.querySelector('button').addEventListener('click', getPic);
 
-
+//Function that fetches the API
 function getPic(){
     day = document.getElementById('calendar').value
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=BEIi4NNCAww71WvZ6aqQgAa2eKK6LlhAhmH80wik&date=${day}`)
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=BEIi4NNCAww71WvZ6aqQgAa2eKK6LlhAhmH80wik&date=${getVar()}`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
@@ -20,41 +24,25 @@ function getPic(){
             desc.innerText = data.explanation; 
             currentDate = data.date;
             if(slideshow.checked == true){
-                slideDelay();
+                setTimeout(getPic, 6000);
             } 
         })
         .catch(err =>{
             console.log(err);
         })
 }
-
-function slideDelay() {
-    setTimeout(startSlideshow, 6000)
+//Determines which variable to use for the API date. The day selected, or the slideshow of pictures
+function getVar() {
+    if(slideshow.checked == true){
+        if(currentDate == undefined){
+            currentDate = day;
+        }
+        return getDayBefore();
+    }else{
+        return day;
+    }
 }
-
-
-
-function startSlideshow() {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=BEIi4NNCAww71WvZ6aqQgAa2eKK6LlhAhmH80wik&date=${getDayBefore()}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            img.src = data.url;
-            h2.innerText = data.title; 
-            desc.innerText = data.explanation; 
-            currentDate = data.date; 
-            if(slideshow.checked == true){
-                slideDelay();
-            }
-        })
-        .catch(err =>{
-            console.log(err);
-        })
-}
-
-
-
-
+//Months Object for use in the getDayBefore function
 const months = {
     01: 31,
     02: 28,
@@ -69,7 +57,7 @@ const months = {
     11: 30,
     12: 31,
 }
-
+//Gets the day before the date of the current API photo
 function getDayBefore(){
     let dateArr = currentDate.split('-');
     console.log(date)
@@ -87,4 +75,3 @@ function getDayBefore(){
 }    
 
 
-// run the function and continue the function if it is checked to loop to another function.
